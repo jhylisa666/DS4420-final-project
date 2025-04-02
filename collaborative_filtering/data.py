5d26a20f38412a7663811821552e57c24cd1b781
Harrison Kim <kim.harr@northeastern.edu>
2025-04-01 21:49:01 -0400

    creating train test split

--- collaborative_filtering/data.py
+++ collaborative_filtering/data.py
@@ -1,6 +1,8 @@
 import pandas as pd
-from utils import cosine_similarity, euclidean_distance
+from utils import cosine_similarity, euclidean_distance, weighted_cosine_similarity
 import category_encoders as ce
+import numpy as np
+from typing import Tuple
 
 RESTAURANT_FEATURES = [
     "placeID",
@@ -15,6 +17,8 @@ RESTAURANT_FEATURES = [
     "parking_lot",
 ]
 
+WEIGHTS = [1.0 for _ in range(len(RESTAURANT_FEATURES) - 1)]
+
 
 def _get_item_similarities(
     df: pd.DataFrame, distance_metric: str = "cosine_similarity"
@@ -28,6 +32,7 @@ def _get_item_similarities(
     assert distance_metric in [
         "euclidean_distance",
         "cosine_similarity",
+        "weighted_cosine_similarity",
     ], f"Distance metric {distance_metric} not supported."
 
     restaurant_1, restaurant_2, similarities = [], [], []
@@ -44,6 +49,10 @@ def _get_item_similarities(
                 )
             elif distance_metric == "cosine_similarity":
                 similarity = cosine_similarity(restaurant_1_vector, restaurant_2_vector)
+            elif distance_metric == "weighted_cosine_similarity":
+                similarity = weighted_cosine_similarity(
+                    restaurant_1_vector, restaurant_2_vector, WEIGHTS
+                )
 
             restaurant_1.append(restaurant_1_id)
             restaurant_2.append(restaurant_2_id)
@@ -58,7 +67,7 @@ def _get_item_similarities(
     )
 
 
-def _preprocess_rating_data(df: pd.DataFrame) -> pd.DataFrame:
+def _preprocess_restaurant_data(df: pd.DataFrame) -> pd.DataFrame:
     """
     Converts all categorical features to numerical values.
     Uses a memory-efficient, lower-dimensional binary encoding for the categorical features.
@@ -85,13 +94,40 @@ def _load_data() -> pd.DataFrame:
     return merged_df
 
 
-def get_train_test_split(df: pd.DataFrame):
-    pass
+def get_train_test_split() -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """
+    Given the ratings data, for each user, it random selects one restaurant to be the test set
+    and the rest to be the training set. The selected restaurant's rating is removed from the training set.
+
+    Returns:
+        - ratings_df (pd.DataFrame): a DataFrame with the training set ratings
+        - test_df (pd.DataFrame): a DataFrame with the test set ratings
+    """
+    user_ids, restaurant_ids, ratings = [], [], []
+    ratings_df = pd.read_csv("../food_data/rating_final.csv")
+    ratings_df = ratings_df.pivot(index="userID", columns="placeID", values="rating")
+
+    for index in ratings_df.index:
+        # Get all the rated restaurants for the current user
+        non_nan_values = ratings_df.loc[index][ratings_df.loc[index].notna()]
+        assert len(non_nan_values) > 0, f"User {index} has no rated restaurants."
+
+        # Randomly select one restaurant to be the test set
+        random_restaurant = np.random.choice(non_nan_values.index)
+        rating = non_nan_values[random_restaurant]
+
+        # Set the rating for the selected restaurant to NaN in the training set
+        ratings_df.loc[index, random_restaurant] = np.nan
+
+        user_ids.append(index)
+        restaurant_ids.append(random_restaurant)
+        ratings.append(rating)
+
+    return ratings_df, pd.DataFrame(
+        {"userID": user_ids, "placeID": restaurant_ids, "rating": ratings}
+    )
 
 
-df = _load_data()
-df = _preprocess_rating_data(df)
-print(df.head())
-similarities = _get_item_similarities(df)
-print(similarities.describe())
-print(similarities.head(20))
+train_df, test_df = get_train_test_split()
+print(train_df.head())
+print(test_df.head())
